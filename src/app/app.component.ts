0bf7f23c86a23fd550c15bb71ae477959d2a9c3d
Gregório <vinicius.gregorio@optera.com.br>
2018-10-03 22:26:11 -0300

    adição do model task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Gerenciador de Tarefas';
+  task: Task = {
+    id: 21,
+    title: 'Enviar orçamento para o cliente X'
+  };
+
+  task2: Task = new Task(22, 'Enviar orçamento para o cliente Y');
+}
+
+export class Task{
+    public id: number;
+    public title: string;
+
+    constructor(id: number, title: string){
+      this.id = id;
+      this.title = title;
+    }
 }
