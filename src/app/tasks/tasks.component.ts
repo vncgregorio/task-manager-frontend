51e92c41c60417b46b43c32af3a99f5de60edc09
Gregório <vinicius.gregorio@optera.com.br>
2018-10-05 00:03:19 -0300

    adição do modelo de Tasks dentro do component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-const TASKS: Array<any> = [
+import { Task } from './shared/task.model';
+
+const TASKS: Array<Task> = [
   {
     id: 1,
     title: 'Fazer tarefa 1'
