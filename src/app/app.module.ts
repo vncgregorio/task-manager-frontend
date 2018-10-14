919c7809a2d8e861b17413a537948b034207dcca
Gregório <vinicius.gregorio@optera.com.br>
2018-10-14 14:15:48 -0300

    adicionando basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
@@ -9,6 +10,13 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TasksDetailComponent } from './tasks/task-detail/task-detail.component';
+
+const ROUTES = RouterModule.forRoot([
+  {
+    path: 'tasks',
+    component: TasksComponent
+  }
+])
 
 @NgModule({
   declarations: [
@@ -21,7 +29,8 @@ import { TasksDetailComponent } from './tasks/task-detail/task-detail.component'
     AngularFontAwesomeModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,    
+    ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
