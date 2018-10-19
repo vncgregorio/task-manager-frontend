4f34d622a57e378fb52415ff211cf84dd942399b
Gregório <vinicius.gregorio@optera.com.br>
2018-10-19 00:48:54 -0300

    adicionando o módulo InMemoryWebApi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,10 @@ import { TaskService } from './tasks/shared/task.service';
 // modules imports
 import { AppRoutingModule } from './app-routing.module';
 
+// in memory web api
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryTaskDataService } from "./in-memory-task-data.service";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +37,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryTaskDataService)
   ],
   providers: [
     TaskService
