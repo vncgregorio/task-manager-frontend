06130a963658241db5086d3d393265c7052a7917
Gregório <vinicius.gregorio@optera.com.br>
2018-10-23 22:32:58 -0300

    troca do método getTask para retornar um Observable

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -24,8 +24,9 @@ export class TaskService{
     return Promise.resolve(TASKS.slice(0, 3));
   }
 
-  public getTask(id: number): Promise<Task>{
-    return this.getTasks()
-      .then((tasks) => tasks.find(task => task.id === id))
+  public getTask(id: number): Observable<Task>{
+    let url = `${this.tasksUrl}/${id}`;
+    return this.http.get(url)
+      .map((response: Response) => response.json().data as Task);
   }
 }
